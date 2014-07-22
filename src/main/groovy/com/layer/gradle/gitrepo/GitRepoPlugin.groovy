ac529b66cbab346c2c3b65108841ede5b10fb4c7
Douglas Rapp <doug@layer.com>
2014-07-22 11:48:06 -0700

    make repo dir configurable
    turns out android studio really hates reading dependencies out of the build directory for some reason. Now we publish to a dot folder in the home directory with an override

--- src/main/groovy/com/layer/gradle/gitrepo/GitRepoPlugin.groovy
+++ src/main/groovy/com/layer/gradle/gitrepo/GitRepoPlugin.groovy
@@ -3,6 +3,7 @@ package com.layer.gradle.gitrepo
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.Task
+import org.gradle.api.UnknownTaskException
 import org.gradle.api.tasks.Exec
 
 /**
@@ -10,60 +11,88 @@ import org.gradle.api.tasks.Exec
  * Created by drapp on 7/16/14.
  */
 class GitRepoPlugin  implements Plugin<Project> {
+    private static final String PUBLISH = "publish";
+
     void apply(Project project) {
         project.extensions.create("githubPublish", GithubPublish)
 
+        // allow declaring special repositories
         if (!project.repositories.metaClass.respondsTo(project.repositories, 'github', String, String, String, String, Object)) {
             project.repositories.metaClass.github = { String org, String repo, String branch = "master", String type = "releases", def closure = null ->
-                String gitUrl = "git@github.com:$org/${repo}.git"
-                def orgDir = project.file("build/repos/" + org)
+                String gitUrl = gitCloneUrl(org, repo)
+                def orgDir = repositoryDir(project, org)
                 addLocalRepo(project, ensureLocalRepo(project, orgDir, repo, gitUrl, branch), type)
             }
         }
         if (!project.repositories.metaClass.respondsTo(project.repositories, 'git', String, String, String, String, Object)) {
             project.repositories.metaClass.git = { String gitUrl, String name, String branch = "master", String type = "releases", def closure = null ->
-                def orgDir = project.file("build/repos/" + name)
+                def orgDir = repositoryDir(project, name)
                 addLocalRepo(project, ensureLocalRepo(project, orgDir, name, gitUrl, branch), type)
             }
         }
-        Task cloneRepo = project.tasks.create("cloneRepo")
-        cloneRepo.doFirst{
-          def org = project.githubPublish.org
-          def repo = project.githubPublish.repo
-          def branch = project.githubPublish.branch
-          String gitUrl = "git@github.com:$org/${repo}.git"
-          ensureLocalRepo(project, project.file("build/repos/" + org), repo, gitUrl, branch)
-        }
-        project.tasks.getByName("publish").dependsOn(cloneRepo)
 
-        Task publishToGithub = project.tasks.create("publishToGithub")
-        publishToGithub.doFirst {
-            def org = project.githubPublish.org
-            def repo = project.githubPublish.repo
-            def gitDir = project.file("build/repos/" + org + "/" + repo)
-            project.exec {
-                executable "git"
-                workingDir gitDir
-                args "add", "*"
+        if(hasPublishTask(project)) {
+            // add a publishToGithub task
+            Task cloneRepo = project.tasks.create("cloneRepo")
+            cloneRepo.doFirst{
+                ensureLocalRepo(
+                        project,
+                        repositoryDir(project, project.githubPublish.org),
+                        project.githubPublish.repo,
+                        gitCloneUrl(project.githubPublish.org, project.githubPublish.repo),
+                        project.githubPublish.branch)
             }
-            project.exec {
-                executable "git"
-                workingDir gitDir
-                args "commit", "-a", "-m", "published artifacts for  ${project.getGroup()} ${project.version}"
+            project.tasks.getByName(PUBLISH).dependsOn(cloneRepo)
+
+            Task publishToGithub = project.tasks.create("publishToGithub")
+            publishToGithub.doFirst {
+                def gitDir = repositoryDir(project, project.githubPublish.org + "/" + project.githubPublish.repo)
+                project.exec {
+                    executable "git"
+                    workingDir gitDir
+                    args "add", "*"
+                }
+                project.exec {
+                    executable "git"
+                    workingDir gitDir
+                    args "commit", "-a", "-m", "published artifacts for  ${project.getGroup()} ${project.version}"
+                }
+                project.exec {
+                    executable "git"
+                    workingDir gitDir
+                    args "push", "-u", "origin", "master"
+                }
             }
-            project.exec {
-                executable "git"
-                workingDir gitDir
-                args "push", "-u", "origin", "master"
-            }
+            publishToGithub.dependsOn(project.tasks.getByName(PUBLISH))
         }
-        publishToGithub.dependsOn(project.tasks.getByName("publish"))
     }
 
     static class GithubPublish {
         def String org = null
         def String repo = null
         def String branch = "master"
+    }
+
+    private static boolean hasPublishTask(project) {
+        try {
+            project.tasks.getByName(PUBLISH)
+            return true;
+        } catch (UnknownTaskException e) {
+            return false;
+        }
+
+    }
+
+    private static File repositoryDir(Project project, String name) {
+        if(project.hasProperty("gitRepositories")) {
+            project.file(project.property("gitRepositories"))
+        } else {
+            project.file("${System.env.HOME}/.gitRepos/${name}")
+        }
+    }
+
+    private static String gitCloneUrl(String org, String repo) {
+        return "git@github.com:$org/${repo}.git"
     }
 
     private static File ensureLocalRepo(Project project, File directory, String name, String gitUrl, String branch) {
